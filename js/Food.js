142b1aab3a4b1135139084513877ce81c0e2bcc7
NashTheFlash11 <71734964+NashTheFlash11@users.noreply.github.com>
2021-06-16 15:00:14 -0400

    Add files via upload

--- js/Food.js
+++ js/Food.js
@@ -34,14 +34,25 @@ class Food{
         imageMode(CENTER);
         image(this.image, 720, 220, 70, 70);
 
-        for(var i = 0; i < this.foodStock; i++){
-            if(i%10 === 0){
-                x = 80;
-                y = y + 50;
-            }
+        // for(var i = 0; i < this.foodStock; i++){
+        //     if(i%10 === 0){
+        //         x = 80;
+        //         y = y + 50;
+        //     }
 
-            image(this.image, x, y, 50, 50);
-            x = x + 30;
+        //     image(this.image, x, y, 50, 50);
+        //     x = x + 30;
+        // }
+
+        if(foodStock != 0){
+            for(var i = 0; i<this.foodStock;i++){
+                if(i%10 === 0){
+                    x = 80;
+                    y = y+50;
+                }
+                image(this.image, x, y, 50, 50);
+                x = x + 30;
+            }
         }
     }
 }
